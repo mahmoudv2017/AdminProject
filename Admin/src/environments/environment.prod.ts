2748717572c380a6113580c207d7aeb62c3c3df3
mahmoudv2017 <mahmoudv2012@gmail.com>
2023-06-25 03:35:19 +0200

    latest

--- Admin/src/environments/environment.prod.ts
+++ Admin/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  baseApi:"https://aklney.onrender.com/",
+  baseApi:"https://akelny20230624003802.azurewebsites.net/api/",
+  ImgUrl : "https://akelny20230624003802.azurewebsites.net/",
   production: true
 };
