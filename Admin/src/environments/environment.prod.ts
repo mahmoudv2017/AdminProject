ca343b2a9431a875f8cf8a7e1074e37295aaa234
mahmoudv2017 <mahmoudv2012@gmail.com>
2023-06-25 09:57:13 +0200

    latest

--- Admin/src/environments/environment.prod.ts
+++ Admin/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseApi:"https://akelny20230625090658.azurewebsites.net/api/",
-  ImgUrl : "https://akelny20230625090658.azurewebsites.net/",
+  baseApi:"https://akelny20230625093658.azurewebsites.net/api/",
+  ImgUrl : "https://akelny20230625093658.azurewebsites.net/",
   production: true
 };
